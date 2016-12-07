e9724cf231482dc7b27742b6e6a2d622393c53ab
Wisbell <westleyisbell@gmail.com>
2016-12-07 15:17:22 -0600

    Implemented delete function which is called when a delete button is clicked

--- script.js
+++ script.js
@@ -4,8 +4,23 @@ function addDefaultMessages(){
     for (var i = 0; i < messageObject.defaultMessageList.length; i++) {
 
         document.querySelector('.messageContainer').insertAdjacentHTML('beforeend', `<div>
-                                                                                        <span>${messageObject.defaultMessageList[i].message}</span> <button>Delete</button>
+                                                                                        <span>${messageObject.defaultMessageList[i].message}</span> <button class="deleteButton">Delete</button>
                                                                                     </div`)
+        document.querySelector("body").addEventListener("click", deleteMessage);
+    }
+}
+
+
+function deleteMessage(e){
+    //console.log("deleteMessage function called")
+    console.log("event ", e)
+
+    if (event.target.className === "deleteButton"){
+        //console.log("delete button clicked")
+        //console.log(event.target.parentElement);
+
+        var removeThisDiv = event.target.parentElement
+        event.target.parentElement.parentNode.removeChild(removeThisDiv);
     }
 }
 
@@ -23,6 +38,10 @@ var parseMessages = function(e) {
   messageObject = JSON.parse(e.target.responseText);
   addDefaultMessages();
 }
+
+
+// delete button event listener
+//document.querySelector(".deleteButton").addEventListener("click", deleteMessage);
 
 //Request to JSON file to get placeholder messages
 var messageRequest = new XMLHttpRequest();
